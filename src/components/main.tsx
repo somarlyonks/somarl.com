fc383fc296f95fd98182385174b72c5f7d167535
sy <somarl@live.com>
2019-01-03 12:01:40 +0800

    refactor(component): add small specific styled weather animation

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import PanelLeft from './panel/left'
 import PanelRight from './panel/right'
+import WeatherAnimation from './weather/animation'
 
 
 export default function Main () {
   return (
     <main>
+      <WeatherAnimation
+        type="sun"
+        size="small"
+        backgroundColor="rgba(#000, 0.4)"
+      />
+
       <div className="container">
         <div className="row">
           <PanelLeft />
--- src/components/weather/widget.tsx
+++ src/components/weather/widget.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import WeatherAnimation, { WeatherSizes, WeatherTypes } from './animation'
+import WeatherAnimation from './animation'
 
 
 export default class WeatherWidget extends React.Component<{}, {}> {
   public render () {
     return (
       <WeatherAnimation
-        type={WeatherTypes.thunder}
-        size={WeatherSizes.normal}
+        type="thunder"
+        size="normal"
         backgroundColor="rgba(#000, 0.2)"
       />
     )
