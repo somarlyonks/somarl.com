0b6bfb282afc0de6e49c074e1315cc4ae5b721c7
sy <somarl@live.com>
2019-01-05 09:33:12 +0800

    chore(page): remove verbose comments

--- page/scripts/start.js
+++ page/scripts/start.js
@@ -1,12 +1,8 @@
 'use strict'
 
-// Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development'
 process.env.NODE_ENV = 'development'
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
   throw err
 })
@@ -26,7 +22,7 @@ const {
   prepareProxy,
   prepareUrls,
 } = require('react-dev-utils/WebpackDevServerUtils')
-const openBrowser = require('react-dev-utils/openBrowser')
+// const openBrowser = require('react-dev-utils/openBrowser')
 const paths = require('../config/paths')
 const config = require('../config/webpack.config.dev')
 const createDevServerConfig = require('../config/webpackDevServer.config')
@@ -39,33 +35,13 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1)
 }
 
-// Tools like Cloud9 rely on this.
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000
 const HOST = process.env.HOST || '0.0.0.0'
 
-if (process.env.HOST) {
-  console.log(
-    chalk.cyan(
-      `Attempting to bind to HOST environment variable: ${chalk.yellow(
-        chalk.bold(process.env.HOST)
-      )}`
-    )
-  )
-  console.log(
-    'If this was unintentional, check that you haven\'t mistakenly set it in your shell.'
-  )
-  console.log(`Learn more here: ${chalk.yellow('http://bit.ly/2mwWSwH')}`)
-  console.log()
-}
-
-// We attempt to use the default port but if it is busy, we offer the user to
-// run on a different port. `choosePort()` Promise resolves to the next free port.
 choosePort(HOST, DEFAULT_PORT)
   .then(port => {
-    if (port === null) {
-      // We have not found a port.
-      return
-    }
+    if (port === null) return
+
     const protocol = process.env.HTTPS === 'true' ? 'https' : 'http'
     const appName = require(paths.appPackageJson).name
     const urls = prepareUrls(protocol, HOST, port)
@@ -89,10 +65,10 @@ choosePort(HOST, DEFAULT_PORT)
         clearConsole()
       }
       console.log(chalk.cyan('Starting the development server...\n'))
-      openBrowser(urls.localUrlForBrowser)
-    });
+      // openBrowser(urls.localUrlForBrowser)
+    })
 
-    ['SIGINT', 'SIGTERM'].forEach(function (sig) {
+    ; ['SIGINT', 'SIGTERM'].forEach(function (sig) {
       process.on(sig, function () {
         devServer.close()
         process.exit()
