7e177eb6e275fb22ab6a06ebd406184af4a7a4f3
sy <somarl@live.com>
2019-01-07 17:33:43 +0800

    chore(pkg): introduce monorepo with lerna
    jest and circleci configs are also updated

--- packages/page/jest.config.js
+++ packages/page/jest.config.js
@@ -1,51 +1,53 @@
 module.exports = {
-  'collectCoverageFrom': [
+  collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts'
   ],
   // 'roots': [
   //   '<rootDir>/src/test'
   // ],
-  'setupFiles': [
+  setupFiles: [
     '<rootDir>/config/polyfills.js'
   ],
   // 'testMatch': [
   //   '<rootDir>/src/**/__tests__/**/*.(j|t)s?(x)',
   //   '<rootDir>/src/**/?(*.)(spec|test).(j|t)s?(x)'
   // ],
-  'testRegex': '(/src/test/.*|(\\.|/)(test|spec))\\.tsx?$',
-  'testEnvironment': 'jsdom', // node
-  'testURL': 'http://localhost',
-  'transform': {
+  testRegex: '(/src/test/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testEnvironment: 'jsdom', // node
+  testURL: 'http://localhost',
+  transform: {
     // @see https://jestjs.io/docs/en/webpack
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
     '^(?!.*\\.(js|jsx|mjs|css|json)$)': '<rootDir>/config/jest/fileTransform.js'
   },
-  'transformIgnorePatterns': [
+  transformIgnorePatterns: [
     '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$'
   ],
-  'moduleNameMapper': {
+  moduleNameMapper: {
     '^react-native$': 'react-native-web',
     '^src/(.*)': '<rootDir>/src/$1'
   },
-  'moduleFileExtensions': [
-    'web.ts',
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'mjs',
+    'node',
     'ts',
-    'web.tsx',
     'tsx',
     'web.js',
-    'js',
     'web.jsx',
-    'jsx',
-    'json',
-    'node',
-    'mjs'
+    'web.ts',
+    'web.tsx'
   ],
-  'globals': {
+  globals: {
     'ts-jest': {
-      'tsConfigFile': './tsconfig.test.json'
+      'tsConfig': './tsconfig.test.json'
     }
   },
+  preset: 'ts-jest/presets/js-with-babel',
+  testMatch: null,
 }
