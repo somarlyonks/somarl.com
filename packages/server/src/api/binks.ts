3b60ef4705c5d314d99dc9fbfe900987f169c851
sy <somarl@live.com>
2019-01-26 14:33:12 +0800

    feat(server): add meta info for binks

--- packages/server/src/api/binks.ts
+++ packages/server/src/api/binks.ts
@@ -3,28 +3,34 @@ import * as path from 'path'
 import { promisify } from 'util'
 import Router from 'koa-router'
 import { SETTINGS } from '../settings'
-import choice from '../helpers/choice'
+import getDayOfYear from '../helpers/getday'
+import sortdir from '../helpers/sortdir'
 
 
-const readdir = promisify(fs.readdir)
 const readFile = promisify(fs.readFile)
+
+const binksDir = path.resolve(SETTINGS.BINKS_DIR)
+// tslint:disable-next-line:no-var-requires
+const meta: [{copyright: string, image: string}] = require(path.join(binksDir, '..', 'buffer/COPYRIGHTS.json'))
 
 
 /**
- * @helper https://darksky.net/dev/docs
- * @TODO
- *     The singled binks srv directory instead of the passive synced
- *     Dropbox working directory.
- *     Read meta info from COPYRIGHTS.json
+ * @desciption respond different images everyday
+ * @todo move sortdir to daily task
  */
 export default function registerBingRoutes (router: Router) {
   router.get('/binks', async ctx => {
-    const binksDir = path.resolve(SETTINGS.BINKS_DIR)
-
-    const imgs = await readdir(binksDir)
-    const img = await readFile(path.resolve(binksDir, choice(5)(imgs)))
+    const imgs = await sortdir(binksDir)
+    const img = await readFile(path.join(binksDir, imgs[getDayOfYear() % imgs.length]))
 
     ctx.type = 'image/jpg'
     ctx.body = img
+  })
+
+  router.get('/binksinfo', async ctx => {
+    const imgs = await sortdir(binksDir)
+    const img = imgs[getDayOfYear() % imgs.length].split('.')[0]
+
+    ctx.body = meta.find(record => record.image === img) || { copyright: '', image: img }
   })
 }
