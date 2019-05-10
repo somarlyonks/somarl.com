5582d6df013e7300339cbff0ccf1c375e5faacd0
sy <somarl@live.com>
2019-05-10 21:07:19 +0800

    fix(page): fix build and test

--- packages/page/src/test/App.test.tsx
+++ packages/page/src/test/App.test.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import { h, render } from 'preact'
 import App from '../App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  render(<App />, div)
 })
