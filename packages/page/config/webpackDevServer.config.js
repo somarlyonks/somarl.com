014beda3a1ed98d29b691c3b269782be44a53658
sy <somarl@live.com>
2019-05-10 13:12:36 +0800

    chore(pkg): update dependencies and webpack config

--- packages/page/config/webpackDevServer.config.js
+++ packages/page/config/webpackDevServer.config.js
@@ -11,36 +11,20 @@ const host = process.env.HOST || '0.0.0.0'
 
 module.exports = function (proxy, allowedHost) {
   return {
-    // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
-    // websites from potentially accessing local content through DNS rebinding:
     // https://github.com/webpack/webpack-dev-server/issues/887
     // https://medium.com/webpack/webpack-dev-server-middleware-security-issues-1489d950874a
-    disableHostCheck: !proxy ||
-      process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
-    // Enable gzip compression of generated files.
+    disableHostCheck: !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
     compress: true,
-    // Silence WebpackDevServer's own logs since they're generally useless.
-    // It will still show compile warnings and errors with this setting.
     clientLogLevel: 'none',
-    // Instead, we establish a convention that only files in `public` directory
-    // get served. Our build script will copy `public` into the `build` folder.
-    // In `index.html`, you can get URL of `public` folder with %PUBLIC_URL%:
-    // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
-    // In JavaScript code, you can access it with `process.env.PUBLIC_URL`.
     contentBase: paths.appPublic,
-    // By default files from `contentBase` will not trigger a page reload.
     watchContentBase: true,
-    // Enable hot reloading server.
     hot: true,
-    // It is important to tell WebpackDevServer to use the same "root" path
     // as we specified in the config. In development, we always serve from /.
     publicPath: config.output.publicPath,
     // WebpackDevServer is noisy by default so we emit custom message instead
     // by listening to the compiler events with `compiler.plugin` calls above.
     quiet: true,
-    // Reportedly, this avoids CPU overload on some systems.
     // https://github.com/facebookincubator/create-react-app/issues/293
-    // src/node_modules is not ignored to support absolute imports
     // https://github.com/facebookincubator/create-react-app/issues/1065
     watchOptions: {
       ignored: ignoredFiles(paths.appSrc),
@@ -50,7 +34,6 @@ module.exports = function (proxy, allowedHost) {
     host,
     overlay: false,
     historyApiFallback: {
-      // Paths with dots should still use the history fallback.
       // See https://github.com/facebookincubator/create-react-app/issues/387.
       disableDotRule: true,
     },
