382da60931c1e2deabaa6deb5e63eb37421dd282
sy <somarl@live.com>
2019-09-16 16:00:34 +0800

    feat(page): add basic button

---
+++ packages/page/src/components/form/button.tsx
@@ -0,0 +1,37 @@
+import { h, JSX } from 'preact'
+
+
+interface IButtonProps {
+  label: S
+  classNames?: L<S>
+  labelClassName?: S
+  onClick?: JSX.MouseEventHandler
+  style?: string | {[key: string]: string | number}
+}
+
+/**
+ * @description magic words: 'primary', 'borderless'
+ * @example
+ *   <Button label="primary" classNames={['primary', 'borderless']} />
+ */
+const Button = ({
+  label,
+  classNames = [],
+  labelClassName = '',
+  onClick = () => {},
+  style = '',
+}: IButtonProps) => (
+  <button
+    onClick={onClick}
+    className={'fabric-btn ' + classNames.map(s => `${s} fabric-btn--${s}`).join(' ')}
+    style={style}
+  >
+    <span class="fabric-wrapper">
+      <span class="fabric-container">
+        <span class={'fabric-btn__label ' + labelClassName}>{label}</span>
+      </span>
+    </span>
+  </button>
+)
+
+export default Button
