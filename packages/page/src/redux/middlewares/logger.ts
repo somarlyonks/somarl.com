77280ba7cae66d7a7a161b70136a7106e0900973
sy <somarl@live.com>
2019-09-29 11:35:47 +0800

    feat(page): redux logger middleware ignore support

--- packages/page/src/redux/middlewares/logger.ts
+++ packages/page/src/redux/middlewares/logger.ts
@@ -1,21 +1,33 @@
 import { IDispatcher, IAction, IMiddlewareAPI } from '../framework'
 
 
+interface ILoggerMiddlewareOptions {
+  ignores?: L<S>
+}
+
 /** @description Temporary logger for debug in console. */
-export function loggerMiddleware <TState, TAction extends IAction> ({ getState }: IMiddlewareAPI<TState, TAction>) {
-  return (next: IDispatcher<TAction>) => (action: TAction) => {
-    const start = performance.now()
-    const prevState = getState()
-    const ret = next(action)
-    const nextState = getState()
-    const took = performance.now() - start
+export class LoggerMiddleware {
+  public static configure ({ ignores = [] }: ILoggerMiddlewareOptions = {}) {
+    return function loggerMiddleware <TState, TAction extends IAction> (
+      { getState }: IMiddlewareAPI<TState, TAction>
+    ) {
+      return (next: IDispatcher<TAction>) => (action: TAction) => {
+        if (ignores.includes(action.type)) return next(action)
 
-    console.groupCollapsed('[redux]: action', action.type, `in ${took.toFixed(2)}ms`)
-    console.info('[redux prev state]:', prevState)
-    console.info('[redux action]:', action)
-    console.info('[redux next state]:', nextState)
-    console.groupEnd()
+        const start = performance.now()
+        const prevState = getState()
+        const ret = next(action)
+        const nextState = getState()
+        const took = performance.now() - start
 
-    return ret
+        console.groupCollapsed('[redux]: action', action.type, `in ${took.toFixed(2)}ms`)
+        console.info('[redux prev state]:', prevState)
+        console.info('[redux action]:', action)
+        console.info('[redux next state]:', nextState)
+        console.groupEnd()
+
+        return ret
+      }
+    }
   }
 }
