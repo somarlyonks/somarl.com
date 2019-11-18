d79c139e5e087b3167fece53ec41ae7e849d170e
sy <somarl@live.com>
2019-11-18 14:27:57 +0800

    feat(page): add cross icon

--- packages/page/src/components/icons/index.ts
+++ packages/page/src/components/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as DarkSky } from './darksky'
+export { default as Cross } from './cross'
 export { default as Loading } from './loading'
 export { default as RainDrops } from './raindrops'
 export { default as Umbrella } from './umbrella'
