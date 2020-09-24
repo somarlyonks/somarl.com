dfd8d14abe2b6f93c973a0e2a06aacb8c89b8bc2
sy <somarl@live.com>
2020-09-24 15:07:11 +0800

    feat(page): add rerender hook

--- packages/page/src/helpers/hooks.ts
+++ packages/page/src/helpers/hooks.ts
@@ -1,5 +1,5 @@
 
-import { Inputs, useMemo, useState } from 'preact/hooks'
+import { Inputs, useMemo, useReducer, useState } from 'preact/hooks'
 import { bem } from '@somarlyonks/bem'
 
 export function useBoolState (initialState = false): [boolean, F0, F0] {
@@ -23,4 +23,11 @@ export function useBem (block: S, element: S, modifiers: IModifiers = [], inputs
     console.info('recalc class')
     return bem(block, element, modifiers)
   }, inputs)
+}
+
+
+export function useRerender () {
+  const [, rerender] = useReducer(x => x + 1, 0)
+
+  return rerender as F0<void>
 }
