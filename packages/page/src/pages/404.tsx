605b8788fdd65e0692398966dc4be9931364e3df
sy <somarl@live.com>
2021-08-05 11:08:02 +0800

    chore(page): extract 404 page

---
+++ packages/page/src/pages/404.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+import FOF from '@csszen/components.fof'
+
+import Head from 'src/components/head'
+
+
+export default function CountentNotFound () {
+    return <FOF Head={Head} Link={Link} />
+}
