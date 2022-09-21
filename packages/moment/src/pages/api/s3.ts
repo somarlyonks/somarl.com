789143a6c8ba38147a70546dd3bca7dd85dd0b7d
sy <somarl@live.com>
2022-09-21 17:43:26 +0800

    fix(moment): fix s3 api

--- packages/moment/src/pages/api/s3.ts
+++ packages/moment/src/pages/api/s3.ts
@@ -4,6 +4,8 @@ import {createPresignedPost} from '../../libs/s3'
 
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (!req.query.key || !req.query.accesskey) return res.status(HTTPStatusCodes.BAD_REQUEST).end()
+
     const r = await createPresignedPost(
         {key: req.query.key.toString()},
         req.query.accesskey.toString()
