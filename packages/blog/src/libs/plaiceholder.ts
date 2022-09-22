7cbf55761caff44eeba5642847cb0332dfab9edc
sy <somarl@live.com>
2022-09-22 10:17:50 +0800

    fix(blog): fix build error caused by plaiceholder failure

--- packages/blog/src/libs/plaiceholder.ts
+++ packages/blog/src/libs/plaiceholder.ts
@@ -27,41 +27,55 @@ type PostFigureNode = {
 }
 
 async function editImageNode (node: ImageNode): Promise<void> {
-    const {base64, img} = await getPlaiceholder(decodeURI(node.properties.src))
+    try {
+        const {base64, img} = await getPlaiceholder(decodeURI(node.properties.src))
 
-    node.properties.width = img.width
-    node.properties.height = img.height
+        node.properties.width = img.width
+        node.properties.height = img.height
 
-    node.properties.blurDataURL = base64
-    node.properties.placeholder = 'blur'
+        node.properties.blurDataURL = base64
+        node.properties.placeholder = 'blur'
+    } catch (error) {
+        /**
+         * some expected errors are thrown here, so we just ignore them
+         * Pictures from like https://picsum.photos/800/400 could be looked up as unsppoorted file type
+         * See more details in https://github.com/joe-bell/plaiceholder/issues/124#issuecomment-878808923
+         * Same for editPostFigureNode
+         */
+        console.error(error)
+    }
 }
 
 async function editPostFigureNode (node: PostFigureNode): Promise<void> {
     const srcAttribute = node.attributes.find(({name}) => name === 'src')
 
     if (!srcAttribute) return
-    const {base64, img} = await getPlaiceholder(decodeURI(srcAttribute.value))
+    try {
+        const {base64, img} = await getPlaiceholder(decodeURI(srcAttribute.value))
 
-    node.attributes.push({
-        type: 'mdxJsxAttribute',
-        name: 'width',
-        value: img.width,
-    })
-    node.attributes.push({
-        type: 'mdxJsxAttribute',
-        name: 'height',
-        value: img.height,
-    })
-    node.attributes.push({
-        type: 'mdxJsxAttribute',
-        name: 'blurDataURL',
-        value: base64,
-    })
-    node.attributes.push({
-        type: 'mdxJsxAttribute',
-        name: 'placeholder',
-        value: 'blur',
-    })
+        node.attributes.push({
+            type: 'mdxJsxAttribute',
+            name: 'width',
+            value: img.width,
+        })
+        node.attributes.push({
+            type: 'mdxJsxAttribute',
+            name: 'height',
+            value: img.height,
+        })
+        node.attributes.push({
+            type: 'mdxJsxAttribute',
+            name: 'blurDataURL',
+            value: base64,
+        })
+        node.attributes.push({
+            type: 'mdxJsxAttribute',
+            name: 'placeholder',
+            value: 'blur',
+        })
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 export const rehypePlaiceholder: Plugin = () => {
