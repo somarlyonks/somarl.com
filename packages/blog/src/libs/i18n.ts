8883953dbd1ce8b9c1f08663facf4c9bbafad7d1
sy <somarl@live.com>
2022-09-22 10:19:43 +0800

    feat(blog): cut out publish datetime if not specified

--- packages/blog/src/libs/i18n.ts
+++ packages/blog/src/libs/i18n.ts
@@ -1,5 +1,7 @@
 export const formatDatetime = (s: string) => {
     const date = new Date(s)
+    if (!date.getMilliseconds()) return date.toLocaleDateString()
+
     const timeString = date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false})
     return `${date.toLocaleDateString()} ${timeString}`
 }
