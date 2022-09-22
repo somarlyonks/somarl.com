0389ef34de9016d4460e7de1a10d5bef8dc5bf0e
sy <somarl@live.com>
2022-09-22 12:01:24 +0800

    feat(blog): improve post image zoom margin in mobile

--- packages/blog/src/components/Zoom.tsx
+++ packages/blog/src/components/Zoom.tsx
@@ -7,7 +7,7 @@ export default function Zoom () {
             __html: `
             import zoom from "https://cdn.skypack.dev/pin/medium-zoom@v1.0.6-cjz5cHtSkbNjacAAqzaz/mode=imports/optimized/medium-zoom.js";
             zoom("figure[role='img'] > img", {
-                margin: 20,
+                margin: parseFloat(getComputedStyle(document.querySelector('article')).getPropertyValue('font-size')) || 20,
                 background: 'var(--color-bg-transparent)',
             });
         `}} />
