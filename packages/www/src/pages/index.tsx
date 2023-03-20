0d9e2d3a8bfd5785bc1d248b48fa4990179ac2d6
sy <somarl@live.com>
2023-03-20 15:30:41 +0800

    chore(www): change Sitel and Sykes are Foundever now

--- packages/www/src/pages/index.tsx
+++ packages/www/src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Index () {
                 <Avatars />
                 <h1>Hi<span className="m-hidden"> there</span><AH text=" " /><AH className="wave" text="👋" /></h1>
                 <p>I'm Yang. Or <ruby>盛<rp>(</rp><rt>shèng</rt><rp>)</rp>阳<rp>(</rp><rt>yáng</rt><rp>)</rp></ruby>, in Chinese.</p>
-                <p>I'm a Web developer for <a href="https://www.sitel.com" target="_blank">Sitel</a> based in Shanghai.</p>
+                <p>I'm a Web developer <a href="https://foundever.com" target="_blank">@Foundever</a> based in Shanghai.</p>
                 <p>I speak <AH text="🀄️ " />Chinese, <AH text="🔤 " />English, <AH text="🌸 " />Japanese, <AH text="🐍 " />Python, <Javascript />Javascript, <AH text="🦀 " />Rust...</p>
                 <p>I compose <AH text="💻 " />programs and <AH text="🎹 " />music and have been teaching myself computer science and playing paino for years.</p>
             </article>
