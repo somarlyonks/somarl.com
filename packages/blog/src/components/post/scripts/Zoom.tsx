59ec2c1c3d676ff8d5a92f4b43c32ab67bbd15b3
sy <somarl@live.com>
2023-06-15 00:15:43 +0800

    chore(blog): fix skypack is dead

--- packages/blog/src/components/post/scripts/Zoom.tsx
+++ packages/blog/src/components/post/scripts/Zoom.tsx
@@ -4,7 +4,7 @@ import PostScript from './PostScript'
 export default function Zoom () {
     return (
         <PostScript script={`
-            import zoom from "https://cdn.skypack.dev/pin/medium-zoom@v1.0.6-cjz5cHtSkbNjacAAqzaz/mode=imports/optimized/medium-zoom.js";
+            import zoom from "https://esm.sh/medium-zoom@1";
             zoom("figure[role='img'] > img", {
                 margin: parseFloat(getComputedStyle(document.querySelector('article')).getPropertyValue('font-size')) || 20,
                 background: 'var(--color-bg-transparent)',
