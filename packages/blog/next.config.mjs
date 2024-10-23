288a89c25c27e706f5ee7d8d61c26dc506988a07
sy <somarl@live.com>
2024-10-23 19:22:25 +0800

    chore(blog): update next image configs

--- packages/blog/next.config.mjs
+++ packages/blog/next.config.mjs
@@ -2,7 +2,16 @@
 export default {
     transpilePackages: ["next-mdx-remote"],
     images: {
-        domains: ['s3.us-east-2.amazonaws.com', 'picsum.photos'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's3.us-east-2.amazonaws.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+        ],
         deviceSizes: [1001, 10000],
         imageSizes: [1000, 2000],
     },
