6c4dbdd18d72e1ed906059faff415eb1948093bd
sy <somarl@live.com>
2024-11-26 17:35:16 +0800

    chore(pkg): update next configs

--- packages/blog/next.config.mjs
+++ packages/blog/next.config.mjs
@@ -1,6 +1,9 @@
 
 export default {
-    transpilePackages: ["next-mdx-remote"],
+    transpilePackages: [
+        "next-mdx-remote",
+        "@somarl.com/icons",
+    ],
     images: {
         remotePatterns: [
             {
