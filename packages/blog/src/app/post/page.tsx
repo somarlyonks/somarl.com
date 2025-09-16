f51b70eb4d49643e8a39c85db4a923906dd4ed21
sy <somarl@live.com>
2025-09-16 09:35:09 +0800

    chore(blog): remove prisma

--- packages/blog/src/app/post/page.tsx
+++ packages/blog/src/app/post/page.tsx
@@ -1,7 +1,6 @@
 import {cookies} from 'next/headers'
 import {redirect, notFound} from 'next/navigation'
 
-import {fetchPrisma} from '@/libs/prisma'
 import PostEdit from '@/components/post/edit/PostEdit'
 
 interface IProps {
@@ -13,34 +12,9 @@ export default async function Page ({searchParams}: {searchParams: Promise<IProp
     const cookieStore = await cookies()
     const isLoggedIn = !!cookieStore.get('tokens.accessTokenName')
     if (!isLoggedIn) redirect('/login?redirect=/post')
-
-    if (id) {
-        const {content} = await getPost(id)
-
-        return (
-            <PostEdit content={content} />
-        )
-    }
-
-    await getPosts()
+    if (!id) notFound()
 
     return (
         <PostEdit />
     )
 }
-
-const getPosts = async () => fetchPrisma(async (prisma) => {
-    return prisma.post.findMany()
-})
-
-const getPost = async (id: string) => fetchPrisma(async (prisma) => {
-    try {
-        return await prisma.post.findUniqueOrThrow({
-            where: {
-                id: Number(id),
-            },
-        })
-    } catch {
-        notFound()
-    }
-})
